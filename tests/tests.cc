8a55771845ab31eab6b9027a8f001b51b4419a06
KavishPatel <kavishp2@illinois.edu>
2025-10-03 14:43:25 -0500

    test 4

--- tests/tests.cc
+++ tests/tests.cc
@@ -75,7 +75,7 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
 }
 
-TEST_CASE("Example: Simple deposit", "[ex-2]") {
+TEST_CASE("Example: Simple deposit", "[ex-4]") {
   Atm atm;
   atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
   atm.DepositCash(12345678, 1234, 20);
@@ -84,7 +84,7 @@ TEST_CASE("Example: Simple deposit", "[ex-2]") {
 
   REQUIRE(sam_account.balance == 320.30);
 }
-TEST_CASE("Example: decimal deposit", "[ex-2]") {
+TEST_CASE("Example: decimal deposit", "[ex-5]") {
   Atm atm;
   atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
   atm.DepositCash(12345678, 1234, 20.50);
@@ -92,4 +92,35 @@ TEST_CASE("Example: decimal deposit", "[ex-2]") {
   Account sam_account = accounts[{12345678, 1234}];
 
   REQUIRE(sam_account.balance == 320.80);
+}
+
+TEST_CASE("Example: existing account", "[ex-6]") {
+  Atm atm;
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  auto accounts = atm.GetAccounts();
+  REQUIRE(accounts.size() == 1);
+
+}
+TEST_CASE("Example: widthdraw negative", "[ex-7]") {
+  Atm atm;
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  atm.WithdrawCash(12345678, 1234, -20);
+  auto accounts = atm.GetAccounts();
+  Account sam_account = accounts[{12345678, 1234}];
+
+  REQUIRE(sam_account.balance == 300.30);
+}
+TEST_CASE("Example: non existing ledger", "[ex-8]") {
+  Atm atm;
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  auto& transactions = atm.GetTransactions();
+  transactions[{12345678, 1234}].push_back(
+      "Withdrawal - Amount: $200.40, Updated Balance: $99.90");
+  transactions[{12345678, 1234}].push_back(
+      "Deposit - Amount: $40000.00, Updated Balance: $40099.90");
+  transactions[{12345678, 1234}].push_back(
+      "Deposit - Amount: $32000.00, Updated Balance: $72099.90");
+  atm.PrintLedger("./prompt.txt", 12345678, 1234);
+  REQUIRE(!CompareFiles("./ex-2.txt", "./prompt.txt"));
 }
