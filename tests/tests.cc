ef2bdbc5b8b8d491cd821956274a94d3cc412512
KavishPatel <kavishp2@illinois.edu>
2025-10-03 15:02:21 -0500

    non existing ledger 2

--- tests/tests.cc
+++ tests/tests.cc
@@ -112,14 +112,6 @@ TEST_CASE("Example: widthdraw negative", "[ex-7]") {
 }
 TEST_CASE("Example: non existing ledger", "[ex-8]") {
   Atm atm;
-  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
-  auto& transactions = atm.GetTransactions();
-  transactions[{12345678, 1234}].push_back(
-      "Withdrawal - Amount: $200.40, Updated Balance: $99.90");
-  transactions[{12345678, 1234}].push_back(
-      "Deposit - Amount: $40000.00, Updated Balance: $40099.90");
-  transactions[{12345678, 1234}].push_back(
-      "Deposit - Amount: $32000.00, Updated Balance: $72099.90");
   atm.PrintLedger("./prompt.txt", 12345678, 1234);
-  REQUIRE(!CompareFiles("./ex-2.txt", "./prompt.txt"));
+  REQUIRE(!CompareFiles("./ex-1.txt", "./prompt.txt"));
 }
